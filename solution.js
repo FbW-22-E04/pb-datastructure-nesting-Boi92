a22b248e13a5cf95d2233349d6e5da7637e7f160
Boi Schrader <boi1992@t-online.de>
2022-08-16 16:23:34 +0200

    done

---
+++ solution.js
@@ -0,0 +1,28 @@
+//1 
+let board = [
+    [1, 2, 3],
+    ["quick", "brown", "fox", "jumped", "over", "lazy", "dog"],
+    [true, false]
+  ];
+
+for (let i = 0; i < board.length; i++){
+    for (let j = 0; j < board[i].length; j++){
+        console.log(board[i][j]);
+    }
+}
+
+//2
+
+const doge = {
+    name: "Walter",
+    breed: "Sausage Dog",
+    favoriteFoods: ["fries","treats","dog-food"],
+    printFavFood(){
+        this.favoriteFoods.forEach(foodItem => console.log(foodItem));
+    }
+}
+console.log(doge.favoriteFoods[1]);
+doge.printFavFood();
+
+//3
+
